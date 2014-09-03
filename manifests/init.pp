7f7152f86be04f2c71b7fc927c47f0dec9de800b
Gary A. Stafford <garystafford@rochester.rr.com>
2014-09-02 23:36:00 -0400

    Now working for first time - wget, tar, schema, data!

--- manifests/init.pp
+++ manifests/init.pp
@@ -10,18 +10,41 @@
 #
 # Sample Usage:
 #
-class install_sakila_db {
-  wget::fetch { 'http://downloads.mysql.com/docs/sakila-db.tar.gz':
-    destination => '/tmp/sakila-db.tar.gz',
-    cache_dir   => '/var/cache/wget',
-  }
+class install_sakila_db (
+  $destination_dir = '/tmp',
+  $database        = 'sakila-db',
+  $mysql_command   = 'mysql -u root -e',
+  $account         = 'root') {
+  Exec {
+    path => ['/bin', '/usr/bin'], }
 
+  # for debug output on the puppet master
+  notice("destination_dir: ${destination_dir}")
+  notice("database: ${database}")
+  notice("mysql_command: ${mysql_command}")
+
+  wget::fetch { "http://downloads.mysql.com/docs/${database}.tar.gz":
+    destination => "${destination_dir}/${database}.tar.gz",
+    cache_dir   => '/var/cache/wget',
+  } ->
   exec { 'sakila_tar':
-    command => 'tar zxvf sakila-db.tar.gz',
-    cwd     => '/tmp',
-    path    => ['/bin', 'usr/bin'],
-    user    => 'root',
-    group   => 'root',
-    creates => "/tmp/sakila-db"
+    onlyif  => 'sss',
+    command => "tar zxvf ${database}.tar.gz",
+    cwd     => $destination_dir,
+    user    => $account,
+    group   => $account,
+    creates => "${destination_dir}/${database}"
+  } ->
+  exec { 'sakila_schema':
+    command => "${mysql_command} \"SOURCE ${destination_dir}/${database}/sakila-schema.sql;\"",
+    cwd     => $destination_dir,
+    user    => $account,
+    group   => $account,
+  } ->
+  exec { 'sakila_data':
+    command => "${mysql_command} \"SOURCE ${destination_dir}/${database}/sakila-data.sql;\"",
+    cwd     => $destination_dir,
+    user    => $account,
+    group   => $account,
   }
 }
