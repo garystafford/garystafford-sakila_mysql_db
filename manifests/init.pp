2a37d2a2a2f3eb34418b789b3acf9b1beb75aaf2
Gary A. Stafford <garystafford@rochester.rr.com>
2014-09-05 23:34:10 -0400

    Minor refactor of init.pp

--- manifests/init.pp
+++ manifests/init.pp
@@ -11,38 +11,37 @@
 # Sample Usage:
 #
 class install_sakila_db (
-  $destination_dir   = '/tmp',
-  $database          = 'sakila-db',
-  $mysql_generic_cmd = 'mysql -u root -e',
-  $account           = 'root') {
+  $download_dir = '/tmp',
+  $database_dir = 'sakila-db',
+  $mysql_cmd    = 'mysql -u root -e',
+  $account      = 'root') {
   Exec {
     path => ['/bin', '/usr/bin'], }
 
   include mysql::server
 
-  # service { "mysql": ensure => "running", } ->
-  wget::fetch { "http://downloads.mysql.com/docs/${database}.tar.gz":
-    destination => "${destination_dir}/${database}.tar.gz",
+  wget::fetch { "http://downloads.mysql.com/docs/${database_dir}.tar.gz":
+    destination => "${download_dir}/${database_dir}.tar.gz",
     cache_dir   => '/var/cache/wget',
   } ->
   exec { 'sakila_tar':
-    command => "tar zxvf ${database}.tar.gz",
-    cwd     => $destination_dir,
+    command => "tar zxvf ${database_dir}.tar.gz",
+    cwd     => $download_dir,
     user    => $account,
     group   => $account,
-    creates => "${destination_dir}/${database}"
+    creates => "${download_dir}/${database_dir}"
   } ->
-  exec { 'sakila_schema':
-    onlyif  => "${$mysql_generic_cmd} \"SELECT EXISTS(SELECT SCHEMA_NAME FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME = 'sakila');\"",
-    command => "${$mysql_generic_cmd} \"SOURCE ${destination_dir}/${database}/sakila-schema.sql;\"",
-    cwd     => $destination_dir,
+  exec { 'sakila_import_schema':
+    onlyif  => "${$mysql_cmd} \"SELECT EXISTS(SELECT SCHEMA_NAME FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME = 'sakila');\"",
+    command => "${$mysql_cmd} \"SOURCE ${download_dir}/${database_dir}/sakila-schema.sql;\"",
+    cwd     => $download_dir,
     user    => $account,
     group   => $account,
   } ->
-  exec { 'sakila_data':
-    onlyif  => "${$mysql_generic_cmd} \"SELECT EXISTS(SELECT 1 FROM sakila.store);\"",
-    command => "${$mysql_generic_cmd} \"SOURCE ${destination_dir}/${database}/sakila-data.sql;\"",
-    cwd     => $destination_dir,
+  exec { 'sakila_import_data':
+    onlyif  => "${$mysql_cmd} \"SELECT EXISTS(SELECT 1 FROM sakila.store);\"",
+    command => "${$mysql_cmd} \"SOURCE ${download_dir}/${database_dir}/sakila-data.sql;\"",
+    cwd     => $download_dir,
     user    => $account,
     group   => $account,
   }
